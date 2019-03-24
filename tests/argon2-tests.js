d9441ce9db5c2c6254349e4f84f15adb393af58b
Brandon Lehmann <brandonlehmann@gmail.com>
2019-03-24 19:15:04 -0400

    Fix spacing

--- tests/argon2-tests.js
+++ tests/argon2-tests.js
@@ -4,7 +4,6 @@ const assert = require('assert')
 
 console.log('')
 console.log('Argon2 Tests')
-console.log('')
 
 const xmrigdata = new Buffer('0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02', 'hex')
 
